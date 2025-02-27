95a697038952871a97bf4defca80991291b69135
mrantikadev <94685047+mrantikadev@users.noreply.github.com>
2025-02-27 14:43:07 +0300

    ITokenService and TokenService for JWT Token generation

---
+++ JwtAuthDemo/Services/TokenService.cs
@@ -0,0 +1,48 @@
+﻿
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace JwtAuthDemo.Services
+{
+    public class TokenService : ITokenService
+    {
+        private readonly IConfiguration _configuration;
+
+        public TokenService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string GenerateToken(string username, List<string> roles)
+        {
+            // Get secret key and generate security key
+            string secret = _configuration["AppSettings:Secret"];
+            var key = Encoding.UTF8.GetBytes(secret);
+            var securityKey = new SymmetricSecurityKey(key);
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            // Add user informations and roles to JWT
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, username),
+                new Claim("username", username)
+            };
+
+            foreach (var role in roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+
+            // Create JWT Token
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.UtcNow.AddHours(2),
+                signingCredentials: credentials
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
