0c393d1b9c402df58c9d57e45fabca8478ac259c
mrantikadev <94685047+mrantikadev@users.noreply.github.com>
2025-02-27 14:53:40 +0300

    SecureController

---
+++ JwtAuthDemo/Controllers/SecureController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JwtAuthDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize] // Only admins can access
+    public class SecureController : ControllerBase
+    {
+        [HttpGet("admin")]
+        [Authorize(Roles = "Admin")]
+        public IActionResult GetAdminData()
+        {
+            return Ok("Only Admin can access to this page!");
+        }
+    }
+}
